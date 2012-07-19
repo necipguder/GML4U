cba364cb4fb8c1b3bc0e5d2b71ad2a656c080304
01010101 <jerome@saint-clair.net>
2012-07-19 22:15:07 +0200

    Adding length information to GmlPoint

--- src/gml4u/model/GmlPoint.java
+++ src/gml4u/model/GmlPoint.java
@@ -11,8 +11,10 @@ public class GmlPoint extends Vec3D {
 	public Vec3D rotation;
 	public Vec3D direction;
 	public float preasure;
+	public float length;
 
 	public static float DEFAULT_PRESSURE = 1;
+	public static float DEFAULT_LENGTH = 1;
 	
 	/**
 	 * Creates a new GmlPoint
@@ -44,6 +46,25 @@ public class GmlPoint extends Vec3D {
 		this.preasure = pressure;
 		this.rotation = new Vec3D(rotation);
 		this.direction = new Vec3D(direction);
+		this.length = DEFAULT_LENGTH;
+	}
+	
+	/**
+	 * Creates a new GmlPoint using the given vector, time, pressure, rotation, direction and length
+	 * @param v - Vec3D
+	 * @param time - float
+	 * @param pressure - float
+	 * @param rotation - Vec3D
+	 * @param direction - Vec3D
+	 * @param length - float
+	 */
+	public GmlPoint(Vec3D v, float time, float pressure, Vec3D rotation, Vec3D direction, float length) {
+		super(v);
+		this.time = time;
+		this.preasure = pressure;
+		this.rotation = new Vec3D(rotation);
+		this.direction = new Vec3D(direction);
+		this.length = length;
 	}
 	
 	/**
@@ -56,6 +77,7 @@ public class GmlPoint extends Vec3D {
 		this.preasure = point.preasure;
 		this.rotation = new Vec3D(point.rotation);
 		this.direction = new Vec3D(point.direction);
+		this.length = point.length;
 	}
 	
 	/**
@@ -68,6 +90,7 @@ public class GmlPoint extends Vec3D {
 		this.preasure = point.preasure;
 		this.rotation = new Vec3D(point.rotation);
 		this.direction = new Vec3D(point.direction);
+		this.length = point.length;
 	}
 	
 	/**
@@ -75,6 +98,6 @@ public class GmlPoint extends Vec3D {
 	 * @return String
 	 */
 	public String toString() {
-		return "v"+super.toString()+" t{"+time+"} preasure {"+preasure+"} rotation "+rotation+"} direction "+direction;
+		return "v"+super.toString()+" time {"+time+"} preasure {"+preasure+"} rotation {"+rotation+"} direction {"+direction+"} length {"+length+"}";
 	}
 }
