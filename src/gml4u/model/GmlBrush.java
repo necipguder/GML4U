2defda53cca22f2750a0527e639f65e8360c9919
01010101 <jerome@saint-clair.net>
2013-02-20 14:42:58 +0100

    Defaut brush set to an unknonw value so we can used the GmlBrushManager default value

--- src/gml4u/model/GmlBrush.java
+++ src/gml4u/model/GmlBrush.java
@@ -50,7 +50,7 @@ public class GmlBrush extends GmlGenericContainer {
 	private void checkMandatoryParameters() {
 		if (null == get(GmlBrush.UNIQUE_STYLE_ID) || "".equals(get(GmlBrush.UNIQUE_STYLE_ID))) {
 			LOGGER.debug("No brush defined, using default Brush instead");
-			set(GmlBrush.UNIQUE_STYLE_ID, GmlBrushManager.BRUSH_DEFAULT);
+			set(GmlBrush.UNIQUE_STYLE_ID, "unknown");
 		}
 	}
 
